a7ab93c9437ec1efb25673e217ce29af450fac02
Andras Hazadi <hazadiand@gmail.com>
2017-04-09 21:24:09 +0200

    MainActivity számláló

--- app/src/main/java/com/chtv/korsoapp/MainActivity.java
+++ app/src/main/java/com/chtv/korsoapp/MainActivity.java
@@ -1,13 +1,50 @@
 package com.chtv.korsoapp;
 
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final TextView ido=(TextView) findViewById(R.id.ido);
+        Button gomb=(Button) findViewById(R.id.gomb);
+        final CountDownTimer asd= new CountDownTimer(5000, 50) {    //az 50 tűnik annak a bűvös számnak ahol a procit nem hajtja mint az állat és folyamatos a számláló
+
+            public void onTick(long millisUntilFinished) {
+                ido.setText("seconds remaining: "+"0"+millisUntilFinished/1000 +":"+ millisUntilFinished % 1000 );
+            }
+
+            public void onFinish() {
+                Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+                r.play();
+                ido.setText("done!");
+            }
+        };
+
+        gomb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                asd.start();
+            }
+        });
+
+
+
+
+
     }
 }
