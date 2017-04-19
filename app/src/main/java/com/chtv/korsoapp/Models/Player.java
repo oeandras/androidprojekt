0a1effea625da22673fbe80eab8834b61b5eca7b
Kristóf Lajos Tengely <Kristf Lajos Tengely>
2017-04-19 22:22:38 +0200

    added empty activities and models

---
+++ app/src/main/java/com/chtv/korsoapp/Models/Player.java
@@ -0,0 +1,13 @@
+package com.chtv.korsoapp.Models;
+
+import java.util.List;
+
+/**
+ * Created by tk95s on 2017. 04. 19..
+ */
+
+public class Player {
+    String name;
+    List<ContestEvent> contestEvents;
+    List<PlayerResult> playerResults;
+}
---
+++ app/src/main/java/com/chtv/korsoapp/Models/PlayerResult.java
@@ -0,0 +1,8 @@
+package com.chtv.korsoapp.Models;
+
+/**
+ * Created by tk95s on 2017. 04. 19..
+ */
+
+public class PlayerResult {
+}
